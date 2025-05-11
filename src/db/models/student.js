915611517a7fc4b01447d03aae4a01daba98f3be
Anton Bryzghalov <absamurai@gmail.com>
2025-05-11 14:20:31 +0300

    Code from lesson

---
+++ src/db/models/student.js
@@ -0,0 +1,26 @@
+import { model, Schema } from 'mongoose';
+
+const studentsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      enum: ['male', 'female', 'other'],
+    },
+    avgMark: {
+      type: Number,
+      required: true,
+    },
+    onDuty: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const StudentsCollection = model('students', studentsSchema);
