a37a7d519f67808dba68b7f998f4f25bf86298fc
Anton Bryzghalov <absamurai@gmail.com>
2025-06-06 23:03:54 +0300

    Fixes to the logic

--- src/utils/parsePaginationParams.js
+++ src/utils/parsePaginationParams.js
@@ -3,11 +3,7 @@ const parseNumber = (number, defaultValue) => {
   if (!isString) return defaultValue;
 
   const parsedNumber = parseInt(number);
-  if (Number.isNaN(parsedNumber)) {
-    return defaultValue;
-  }
-
-  return parsedNumber;
+  return Number.isNaN(parsedNumber) ? defaultValue : parsedNumber;
 };
 
 export const parsePaginationParams = (query) => {
