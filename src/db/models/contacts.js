7b50dab5ec9fb844812a92be368d967feb1b14b1
Anton Bryzghalov <absamurai@gmail.com>
2025-06-22 12:27:32 +0300

    Swagger docs implemented

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -28,7 +28,7 @@ const contactsSchema = new Schema(
       required: true,
       default: 'personal',
     },
-    photo: { type: String },
+    photo: { type: String }, // URL to the photo
   },
   {
     timestamps: true,
